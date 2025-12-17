1ad43eba924724f00b934660c8e90ba58bd68a05
ruddmslee <dlruddms326@naver.com>
2025-12-18 01:25:23 +0900

    Feat: 예외 처리 log 추가

--- src/main/java/com/carelink/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/carelink/backend/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.carelink.backend.global.exception;
 
 import com.carelink.backend.global.response.BaseResponse;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     // 커스텀 에러 (BaseException) 발생한 경우
@@ -26,6 +28,8 @@ public class GlobalExceptionHandler {
     // RequestBody 검증 실패한 경우 (필수 입력 항목 누락 / 형식 오류)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<BaseResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException - " + e.getMessage());
+
         FieldError fieldError = e.getBindingResult().getFieldError();
 
         String message = "필수 입력 항목이 누락되었거나 잘못된 형식입니다.";
@@ -47,6 +51,7 @@ public class GlobalExceptionHandler {
     // 필수 쿼리 파라미터가 누락된 경우
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<BaseResponse<?>> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        log.error("MissingServletRequestParameterException - " + e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.INVALID_INPUT_VALUE.getStatus())
                 .body(BaseResponse.error(ErrorCode.INVALID_INPUT_VALUE, "필수 파라미터가 누락되었습니다."));
@@ -55,6 +60,7 @@ public class GlobalExceptionHandler {
     // 쿼리 파라미터 값이 잘못된 경우
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<BaseResponse<?>> handleConstraintViolationException(ConstraintViolationException e) {
+        log.error("ConstraintViolationException - " + e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.INVALID_INPUT_VALUE.getStatus())
                 .body(BaseResponse.error(ErrorCode.INVALID_INPUT_VALUE, "잘못된 쿼리 파라미터 값입니다."));
@@ -63,6 +69,7 @@ public class GlobalExceptionHandler {
     // 요청 바디가 비어있거나 JSON 형식이 올바르지 않거나 매핑에 실패한 경우 (ex. 잘못된 Enum값)
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<BaseResponse<?>> handleEnumParseError(HttpMessageNotReadableException e) {
+        log.error("HttpMessageNotReadableException - " + e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.INVALID_INPUT_VALUE.getStatus())
                 .body(BaseResponse.error(ErrorCode.INVALID_INPUT_VALUE, "요청 바디가 비어있거나 JSON 형식이 올바르지 않거나 매핑에 실패했습니다."));
