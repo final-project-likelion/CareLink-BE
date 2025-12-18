01b7432d53873b7a29ff04e16052bf0db93964e1
ruddmslee <dlruddms326@naver.com>
2025-12-19 01:09:58 +0900

    Feat: 오늘 약 복용 기록 업데이트 기능 구현

--- src/main/java/com/carelink/backend/medicine/repository/MedicineIntakeLogRepository.java
+++ src/main/java/com/carelink/backend/medicine/repository/MedicineIntakeLogRepository.java
@@ -4,6 +4,14 @@ import com.carelink.backend.medicine.entity.MedicineIntakeLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+
 @Repository
-public interface MedicineIntakeLogRepository extends JpaRepository<MedicineIntakeLog, Integer> {
+public interface MedicineIntakeLogRepository extends JpaRepository<MedicineIntakeLog, Long> {
+    Boolean existsByMedicineIntakeTimeIdAndDate(Long medcineIntakeTimeId, LocalDate date);
+    List<MedicineIntakeLog> findByMedicineIntakeTime_UserMedicine_User_IdAndDate(Long userId, LocalDate date);
+    void deleteByMedicineIntakeTimeIdInAndDate(Set<Long> medicineIntakeTimeId, LocalDate date);
 }
