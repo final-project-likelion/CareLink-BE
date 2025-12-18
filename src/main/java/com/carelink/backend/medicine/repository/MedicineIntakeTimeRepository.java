470fb60067d686215b3225a272bbbd5122cd1c88
ruddmslee <dlruddms326@naver.com>
2025-12-19 01:09:41 +0900

    Feat: 오늘 복용 약 리스트 + 복용 여부 조회 기능 구현

--- src/main/java/com/carelink/backend/medicine/repository/MedicineIntakeTimeRepository.java
+++ src/main/java/com/carelink/backend/medicine/repository/MedicineIntakeTimeRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface MedicineIntakeTimeRepository extends JpaRepository<MedicineIntakeTime, Integer> {
+public interface MedicineIntakeTimeRepository extends JpaRepository<MedicineIntakeTime, Long> {
     List<MedicineIntakeTime> findByUserMedicineId(Long userMedicineId);
+    List<MedicineIntakeTime> findByUserMedicine_User_Id(Long userId);
+    Optional<MedicineIntakeTime> findByUserMedicine_User_IdAndId(Long userId, Long medicineIntakeTimeId);
 }
