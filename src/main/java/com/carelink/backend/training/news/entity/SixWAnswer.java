a88d3bcfb7baac92a60f47f80c1e035347d8d954
minnnnnnni <dkstodtjdals@gmail.com>
2025-12-20 04:22:05 +0900

    Feat: 육하원칙 모범답안 엔티티 구현

--- src/main/java/com/carelink/backend/training/news/entity/SixWAnswer.java
+++ src/main/java/com/carelink/backend/training/news/entity/SixWAnswer.java
@@ -1,4 +1,56 @@
 package com.carelink.backend.training.news.entity;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SixWAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "news_id", nullable = false)
+    private News news;
+
+    @Column(nullable = false, length = 255)
+    private String who;
+
+    @Column(nullable = false, length = 255)
+    private String whenAt;
+
+    @Column(nullable = false, length = 255)
+    private String whereAt;
+
+    @Column(nullable = false, length = 500)
+    private String what;
+
+    @Column(nullable = false, length = 500)
+    private String why;
+
+    @Column(nullable = false, length = 255)
+    private String how;
+
+    public SixWAnswer(
+            News news,
+            String who,
+            String whenAt,
+            String whereAt,
+            String what,
+            String why,
+            String how
+    ) {
+        this.news = news;
+        this.who = who;
+        this.whenAt = whenAt;
+        this.whereAt = whereAt;
+        this.what = what;
+        this.why = why;
+        this.how = how;
+    }
 }
