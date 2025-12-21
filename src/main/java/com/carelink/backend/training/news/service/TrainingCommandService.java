1d2fa2468de57a15d08f7ee5bd2de43621c5a221
minnnnnnni <dkstodtjdals@gmail.com>
2025-12-22 01:46:37 +0900

    Feat: 인지훈련 기사 중복훈련 불가, 하루에 두개이상 불가 규정한 서비스

--- src/main/java/com/carelink/backend/training/news/service/TrainingCommandService.java
+++ src/main/java/com/carelink/backend/training/news/service/TrainingCommandService.java
@@ -1,4 +1,35 @@
 package com.carelink.backend.training.news.service;
 
+import com.carelink.backend.training.news.entity.*;
+import com.carelink.backend.training.news.repository.CognitiveTrainingRepository;
+import com.carelink.backend.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class TrainingCommandService {
+
+    private final CognitiveTrainingRepository trainingRepository;
+
+    // 기사요약 2단계 완료 시 호출
+    public void completeTraining(User user, News news) {
+
+        if (trainingRepository.existsByUserIdAndNewsId(user.getId(), news.getId())) {
+            throw new IllegalStateException("이미 훈련한 기사입니다.");
+        }
+
+        LocalDate today = LocalDate.now();
+        if (trainingRepository.existsByUserIdAndCompletedDate(user.getId(), today)) {
+            throw new IllegalStateException("하루에 하나의 훈련만 가능합니다.");
+        }
+
+        trainingRepository.save(
+                new CognitiveTraining(user, news, today)
+        );
+    }
 }
