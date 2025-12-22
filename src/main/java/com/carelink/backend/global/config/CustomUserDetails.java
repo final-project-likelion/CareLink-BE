0eb71998e599574be3b7690e25c60244d6762f4c
ruddmslee <dlruddms326@naver.com>
2025-12-22 15:13:05 +0900

    Chore: CustomUserDetails 생성

---
+++ src/main/java/com/carelink/backend/global/config/CustomUserDetails.java
@@ -0,0 +1,65 @@
+package com.carelink.backend.global.config;
+
+import com.carelink.backend.user.entity.User;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Long id;
+    //private final String username;
+
+//    public CustomUserDetails(User user) {
+//        this.id = user.getId();
+//        this.username = user.getPhoneNum();
+//    }
+
+    public CustomUserDetails(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+//        return username;
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
