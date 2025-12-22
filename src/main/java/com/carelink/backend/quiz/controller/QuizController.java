1b64f0372fa25269e3f4da4410205768c33b90eb
ruddmslee <dlruddms326@naver.com>
2025-12-23 00:57:23 +0900

    Feat: 퀴즈 AuthenticationPrincipal 수정

--- src/main/java/com/carelink/backend/quiz/controller/QuizController.java
+++ src/main/java/com/carelink/backend/quiz/controller/QuizController.java
@@ -1,9 +1,11 @@
 package com.carelink.backend.quiz.controller;
 
+import com.carelink.backend.global.config.CustomUserDetails;
 import com.carelink.backend.global.response.BaseResponse;
 import com.carelink.backend.quiz.dto.*;
 import com.carelink.backend.quiz.service.QuizService;
 import com.carelink.backend.user.entity.User;
+import com.carelink.backend.user.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -14,25 +16,26 @@ import org.springframework.web.bind.annotation.*;
 public class QuizController {
 
     private final QuizService quizService;
+    private final AuthService authService;
 
     @GetMapping("/")
     public BaseResponse<QuizResponseDto> getTodayQuiz(
-            @AuthenticationPrincipal User user
+            @AuthenticationPrincipal CustomUserDetails customUserDetails
     ) {
         return BaseResponse.success(
-                quizService.getTodayQuiz(user)
+                quizService.getTodayQuiz(authService.getCurrentUser(customUserDetails.getId()))
         );
     }
 
 
     @PostMapping("/{quizId}/submit")
     public BaseResponse<QuizSubmitResponseDto> submitQuiz(
-            @AuthenticationPrincipal User user,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @PathVariable Long quizId,
             @RequestBody QuizSubmitRequestDto request
     ) {
         return BaseResponse.success(
-                quizService.submitQuiz(user, quizId, request)
+                quizService.submitQuiz(authService.getCurrentUser(customUserDetails.getId()), quizId, request)
         );
     }
 
