a59811d7abdf77ea22c20b3b7d8565965212fb0d
ruddmslee <dlruddms326@naver.com>
2025-12-22 15:17:44 +0900

    Chore: AuthController Swagger 작성

--- src/main/java/com/carelink/backend/user/controller/AuthController.java
+++ src/main/java/com/carelink/backend/user/controller/AuthController.java
@@ -5,6 +5,8 @@ import com.carelink.backend.user.dto.LoginDto;
 import com.carelink.backend.user.dto.SignUpDto;
 import com.carelink.backend.user.dto.TokenDto;
 import com.carelink.backend.user.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "Auth", description = "인증 관련 API")
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/signup")
+    @Operation(summary = "회원가입")
     public ResponseEntity<BaseResponse<?>> signup(@Valid @RequestBody SignUpDto signUpDto) {
         Long id = authService.signUp(signUpDto);
         return ResponseEntity.ok()
@@ -28,6 +32,7 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @Operation(summary = "로그인")
     public ResponseEntity<BaseResponse<?>> login(@Valid @RequestBody LoginDto loginDto) {
         TokenDto tokens = authService.login(loginDto);
         return ResponseEntity.ok()
@@ -35,6 +40,7 @@ public class AuthController {
     }
 
     @PostMapping("/reissue")
+    @Operation(summary = "토큰 재발급")
     public ResponseEntity<BaseResponse<?>> reissue(@RequestBody String refreshToken) {
         TokenDto tokens = authService.reissue(refreshToken);
         return ResponseEntity.ok()
@@ -42,6 +48,7 @@ public class AuthController {
     }
 
     @PostMapping("/signup/duplicate-check")
+    @Operation(summary = "아이디 중복체크")
     public ResponseEntity<BaseResponse<?>> isPhoneNumDuplicate(@RequestBody String phoneNum) {
         Boolean phoneNumDuplicate = authService.isPhoneNumDuplicate(phoneNum);
         return ResponseEntity.ok()
