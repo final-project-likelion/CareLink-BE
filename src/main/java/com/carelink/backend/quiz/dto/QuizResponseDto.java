bbc30bcfcb03690ea02e93c0c6008b3e637b4cbc
minnnnnnni <dkstodtjdals@gmail.com>
2025-12-22 11:12:33 +0900

    Feat: 퀴즈 dto 작성

---
+++ src/main/java/com/carelink/backend/quiz/dto/QuizResponseDto.java
@@ -0,0 +1,18 @@
+package com.carelink.backend.quiz.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Builder
+public class QuizResponseDto {
+
+    private Long quizId;
+    private LocalDate date;
+    private String question;
+    private List<String> options;
+    private boolean alreadySolved;
+}
---
+++ src/main/java/com/carelink/backend/quiz/dto/QuizSubmitRequestDto.java
@@ -0,0 +1,8 @@
+package com.carelink.backend.quiz.dto;
+
+import lombok.Getter;
+
+@Getter
+public class QuizSubmitRequestDto {
+    private int selectedOption; // 1~4
+}
