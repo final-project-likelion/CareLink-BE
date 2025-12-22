40513dda7b72b4a5939335ad36452b87d4d76169
minnnnnnni <dkstodtjdals@gmail.com>
2025-12-22 11:04:00 +0900

    Feat: 엔티티에 대한 레포지토리 구현

---
+++ src/main/java/com/carelink/backend/quiz/repository/QuizRepository.java
@@ -0,0 +1,11 @@
+package com.carelink.backend.quiz.repository;
+
+import com.carelink.backend.quiz.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    Optional<Quiz> findByDate(LocalDate date);
+}
