44f77c102830e87834153aee39799fbf23f395b6
minnnnnnni <dkstodtjdals@gmail.com>
2025-12-27 18:38:44 +0900

    Feat: 소수점 둘째자리까지 나타내기

--- src/main/java/com/carelink/backend/training/news/dto/MonthlyTrainingSummary.java
+++ src/main/java/com/carelink/backend/training/news/dto/MonthlyTrainingSummary.java
@@ -4,10 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class MonthlyTrainingSummary {
 
-    private int totalCount;
-    private Double averageScore;
-    private Integer maxScore;
+    private final int totalCount;
+    private final Double averageScore;
+    private final Integer maxScore;
+
+    public MonthlyTrainingSummary(
+            int totalCount,
+            Double averageScore,
+            Integer maxScore
+    ) {
+        this.totalCount = totalCount;
+        this.averageScore = roundToTwoDecimalPlaces(averageScore);
+        this.maxScore = maxScore;
+    }
+
+    private Double roundToTwoDecimalPlaces(Double value) {
+        if (value == null) return null;
+        return Math.round(value * 100) / 100.0;
+    }
 }
+
